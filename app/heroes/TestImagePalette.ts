6945719978e2eb069ffca7f097c45b2a7a868340
MuazAhmad7 <ahmad.muaz492@gmail.com>
2025-07-17 10:17:38 -0400

    Add TestImagePalette.ts file for CJML basketball images
    - File was previously ignored by gitignore due to '*test*' pattern
    - Contains image data for SwatchesImageTest component
    - Required for deployment build to succeed

---
+++ app/heroes/TestImagePalette.ts
@@ -0,0 +1,253 @@
+export const testImagePalette = [
+  {
+    imageSrc: '/CJML WEEK 9/DSC00009.jpg',
+    imageAlt: 'CJML Week 9 Photo 1',
+    title: 'CJML Photo 1',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00020.jpg',
+    imageAlt: 'CJML Week 9 Photo 2',
+    title: 'CJML Photo 2',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00043.jpg',
+    imageAlt: 'CJML Week 9 Photo 3',
+    title: 'CJML Photo 3',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00071.jpg',
+    imageAlt: 'CJML Week 9 Photo 4',
+    title: 'CJML Photo 4',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00089.jpg',
+    imageAlt: 'CJML Week 9 Photo 5',
+    title: 'CJML Photo 5',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00125.jpg',
+    imageAlt: 'CJML Week 9 Photo 6',
+    title: 'CJML Photo 6',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00140.jpg',
+    imageAlt: 'CJML Week 9 Photo 7',
+    title: 'CJML Photo 7',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00165.jpg',
+    imageAlt: 'CJML Week 9 Photo 8',
+    title: 'CJML Photo 8',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00171.jpg',
+    imageAlt: 'CJML Week 9 Photo 9',
+    title: 'CJML Photo 9',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00237.jpg',
+    imageAlt: 'CJML Week 9 Photo 10',
+    title: 'CJML Photo 10',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00273.jpg',
+    imageAlt: 'CJML Week 9 Photo 11',
+    title: 'CJML Photo 11',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00351.jpg',
+    imageAlt: 'CJML Week 9 Photo 12',
+    title: 'CJML Photo 12',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00384.jpg',
+    imageAlt: 'CJML Week 9 Photo 13',
+    title: 'CJML Photo 13',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00389.jpg',
+    imageAlt: 'CJML Week 9 Photo 14',
+    title: 'CJML Photo 14',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00418.jpg',
+    imageAlt: 'CJML Week 9 Photo 15',
+    title: 'CJML Photo 15',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00469.jpg',
+    imageAlt: 'CJML Week 9 Photo 16',
+    title: 'CJML Photo 16',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00476.jpg',
+    imageAlt: 'CJML Week 9 Photo 17',
+    title: 'CJML Photo 17',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00482.jpg',
+    imageAlt: 'CJML Week 9 Photo 18',
+    title: 'CJML Photo 18',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00488.jpg',
+    imageAlt: 'CJML Week 9 Photo 19',
+    title: 'CJML Photo 19',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00548.jpg',
+    imageAlt: 'CJML Week 9 Photo 20',
+    title: 'CJML Photo 20',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00579.jpg',
+    imageAlt: 'CJML Week 9 Photo 21',
+    title: 'CJML Photo 21',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00590.jpg',
+    imageAlt: 'CJML Week 9 Photo 22',
+    title: 'CJML Photo 22',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00591.jpg',
+    imageAlt: 'CJML Week 9 Photo 23',
+    title: 'CJML Photo 23',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00601.jpg',
+    imageAlt: 'CJML Week 9 Photo 24',
+    title: 'CJML Photo 24',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00616.jpg',
+    imageAlt: 'CJML Week 9 Photo 25',
+    title: 'CJML Photo 25',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00652.jpg',
+    imageAlt: 'CJML Week 9 Photo 26',
+    title: 'CJML Photo 26',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00665.jpg',
+    imageAlt: 'CJML Week 9 Photo 27',
+    title: 'CJML Photo 27',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00666.jpg',
+    imageAlt: 'CJML Week 9 Photo 28',
+    title: 'CJML Photo 28',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00697.jpg',
+    imageAlt: 'CJML Week 9 Photo 29',
+    title: 'CJML Photo 29',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00711.jpg',
+    imageAlt: 'CJML Week 9 Photo 30',
+    title: 'CJML Photo 30',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00719.jpg',
+    imageAlt: 'CJML Week 9 Photo 31',
+    title: 'CJML Photo 31',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00721.jpg',
+    imageAlt: 'CJML Week 9 Photo 32',
+    title: 'CJML Photo 32',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00751.jpg',
+    imageAlt: 'CJML Week 9 Photo 33',
+    title: 'CJML Photo 33',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00786.jpg',
+    imageAlt: 'CJML Week 9 Photo 34',
+    title: 'CJML Photo 34',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00796.jpg',
+    imageAlt: 'CJML Week 9 Photo 35',
+    title: 'CJML Photo 35',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00797.jpg',
+    imageAlt: 'CJML Week 9 Photo 36',
+    title: 'CJML Photo 36',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00805.jpg',
+    imageAlt: 'CJML Week 9 Photo 37',
+    title: 'CJML Photo 37',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00820.jpg',
+    imageAlt: 'CJML Week 9 Photo 38',
+    title: 'CJML Photo 38',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC09805.jpg',
+    imageAlt: 'CJML Week 9 Photo 39',
+    title: 'CJML Photo 39',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC09874.jpg',
+    imageAlt: 'CJML Week 9 Photo 40',
+    title: 'CJML Photo 40',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC09881.jpg',
+    imageAlt: 'CJML Week 9 Photo 41',
+    title: 'CJML Photo 41',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC09906.jpg',
+    imageAlt: 'CJML Week 9 Photo 42',
+    title: 'CJML Photo 42',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC09944.jpg',
+    imageAlt: 'CJML Week 9 Photo 43',
+    title: 'CJML Photo 43',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC09948.jpg',
+    imageAlt: 'CJML Week 9 Photo 44',
+    title: 'CJML Photo 44',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC09991.jpg',
+    imageAlt: 'CJML Week 9 Photo 45',
+    title: 'CJML Photo 45',
+  },
+  // Reduce to ~50 items for better performance while maintaining smooth wave
+  {
+    imageSrc: '/CJML WEEK 9/DSC00009.jpg',
+    imageAlt: 'CJML Week 9 Photo 46',
+    title: 'CJML Photo 46',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00071.jpg',
+    imageAlt: 'CJML Week 9 Photo 47',
+    title: 'CJML Photo 47',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00125.jpg',
+    imageAlt: 'CJML Week 9 Photo 48',
+    title: 'CJML Photo 48',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00171.jpg',
+    imageAlt: 'CJML Week 9 Photo 49',
+    title: 'CJML Photo 49',
+  },
+  {
+    imageSrc: '/CJML WEEK 9/DSC00273.jpg',
+    imageAlt: 'CJML Week 9 Photo 50',
+    title: 'CJML Photo 50',
+  },
+]; 
